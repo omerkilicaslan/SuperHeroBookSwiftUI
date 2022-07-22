5b8a74f95b4bf73f1de3d2cc3790b5468c4dc6c5
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-22 21:38:26 +0300

    Model Objects created in model file, stored in array

--- SuperHeroBookSwiftUI/Model/SuperHero.swift
+++ SuperHeroBookSwiftUI/Model/SuperHero.swift
@@ -30,4 +30,13 @@ struct Coordinates {
     var longitude : Double
 }
 
-let batman = SuperHero(name: "Batman", realName: "Bruce Wayne", imageName: "batman", city: "Gotham", job: "Businessman", coordinate: Coordinates(latitude: 20.0, longitude: 20.0))
+let batman = SuperHero(name: "Batman", realName: "Bruce Wayne", imageName: "batman", city: "Gotham", job: "Businessman", coordinate: Coordinates(latitude: 41.8, longitude: -87.7))
+
+let superman = SuperHero(name: "Superman", realName: "Clark Kent", imageName: "superman", city: "Kansas", job: "Journalist", coordinate: Coordinates(latitude: 39.08, longitude: -94.7))
+
+let spiderman = SuperHero(name: "Spiderman", realName: "Peter Parker", imageName: "spiderman", city: "New York", job: "Photographer", coordinate: Coordinates(latitude: 40.7, longitude: -74.06))
+
+let ironman = SuperHero(name: "IronMan", realName: "Tony Stark", imageName: "ironman", city: "Los Angeles", job: "Business Man", coordinate: Coordinates(latitude: 33.8, longitude: -118.2))
+
+
+let superHeroArray = [batman, superman, spiderman, ironman]
