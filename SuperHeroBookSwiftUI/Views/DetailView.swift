555919bf6933eec482edddc08a2390605c2dcc55
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-22 22:20:46 +0300

    Detail View Created Successfully

--- SuperHeroBookSwiftUI/Views/DetailView.swift
+++ SuperHeroBookSwiftUI/Views/DetailView.swift
@@ -15,11 +15,56 @@ struct DetailView: View {
         VStack{
             MapView(coordinate: chosenHero.coordinateLocation)
                 .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.3)
+                .edgesIgnoringSafeArea(.all)
+                .overlay {
+                    CustomImageView(image: Image(chosenHero.imageName))
+                        .offset(y: UIScreen.main.bounds.height * 0.125)
+                }.padding(.bottom, 100)
             
-            CustomImageView(image: Image(chosenHero.imageName))
-                .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.3)
-                .offset(y: UIScreen.main.bounds.height * -0.15)
+            VStack{
+                
+                HStack{
+                    Text(chosenHero.name)
+                        .font(.largeTitle)
+                        .foregroundColor(.red)
+                        .bold()
+                        
+                    
+                    Spacer()
+                    
+                    Text(chosenHero.realName)
+                        .font(.largeTitle)
+                        .foregroundColor(.orange)
+                        .bold()
+                        
+
+                }.padding()
+                
+                HStack{
+                    
+                    Text(chosenHero.city)
+                        .font(.title)
+                        .foregroundColor(.orange)
+                        
+                    
+                    Spacer()
+                    
+                    Text(chosenHero.job)
+                        .font(.title)
+                        .foregroundColor(.red)
+                        
+
+                    
+                }.padding()
+            }
+            .padding(.vertical, 75)
+            
+            
+            Spacer()
+            
+                
         }
+        
     }
 }
 
