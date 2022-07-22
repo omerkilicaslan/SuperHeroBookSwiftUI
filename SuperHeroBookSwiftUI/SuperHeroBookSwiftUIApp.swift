503645bb09acb387c2126b243fd8f545b9096fa4
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-22 21:14:34 +0300

    Initial Commit

---
+++ SuperHeroBookSwiftUI/SuperHeroBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroBookSwiftUIApp.swift
+//  SuperHeroBookSwiftUI
+//
+//  Created by Ömer Faruk Kılıçaslan on 22.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
